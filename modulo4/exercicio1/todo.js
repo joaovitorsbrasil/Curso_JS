9a1f7d83f96e42567d4fe1d045931315f4eb1bde
joaovitorsbrasil <joao_v_brasil@estudante.sc.senai.br>
2019-06-26 15:55:09 -0300

    verifica se a idade é maior ou menor que 18 anos

---
+++ modulo4/exercicio1/todo.js
@@ -0,0 +1,20 @@
+
+function checkIdade(idade) {
+
+    return new Promise(function(resolve, rejeita) {
+      setTimeout(function()
+       {
+        return idade >= 18 ? resolve() : rejeita();
+      }, 2000);
+
+    });
+  }
+  
+  checkIdade(20)
+  .then(function() {
+  console.log("Maior que 18");
+  })
+
+  .catch(function() {
+  console.log("Menor que 18");
+  });
